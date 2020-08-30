53e955e70c288ce87a5fb996ec0be3a06ad8ff92
Tisana Roongsawang <tisana@gmail.com>
2020-08-30 12:58:03 +0700

    populate all data and fix selected option bug

--- src/app/cards/card-detail/card-detail.component.ts
+++ src/app/cards/card-detail/card-detail.component.ts
@@ -3,6 +3,7 @@ import {Card} from '../models/card';
 import {ActivatedRoute} from '@angular/router';
 import {CardService} from '../services/card.service';
 import {Location} from '@angular/common';
+import {Category} from '../models/category';
 
 @Component({
   selector: 'app-card-detail',
@@ -11,17 +12,28 @@ import {Location} from '@angular/common';
 })
 export class CardDetailComponent implements OnInit {
   @Input() card: Card;
+  categories: Category[];
+
+  // selectedCategory: number;
 
   constructor(private route: ActivatedRoute, private cardService: CardService, private location: Location) {
   }
 
   ngOnInit(): void {
+    this.getCategories();
     this.getCard();
   }
 
   getCard(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.cardService.getCard(id).subscribe(card => this.card = card);
+    this.cardService.getCard(id).subscribe(card => {
+      this.card = card;
+      // this.selectedCategory = this.card.categoryId;
+    });
+  }
+
+  getCategories(): void {
+    this.cardService.getCategories().subscribe(categories => this.categories = categories);
   }
 
   goBack(): void {
--- src/app/cards/services/card.service.ts
+++ src/app/cards/services/card.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Card} from '../models/card';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {Category} from '../models/category';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +10,20 @@ import {HttpClient} from '@angular/common/http';
 export class CardService {
 
   private REST_API_SERVER = 'http://localhost:8081';
+  private CARD_RESOURCE = this.REST_API_SERVER + '/api/cards';
 
   constructor(private httpclient: HttpClient) {
   }
 
   getCards(): Observable<Card[]> {
-    return this.httpclient.get<Card[]>(this.REST_API_SERVER + '/api/cards');
+    return this.httpclient.get<Card[]>(this.CARD_RESOURCE);
   }
 
   getCard(id: number): Observable<Card> {
-    return this.httpclient.get<Card>(this.REST_API_SERVER + '/api/cards/' + id);
+    return this.httpclient.get<Card>(this.CARD_RESOURCE + '/' + id);
+  }
+
+  getCategories(): Observable<Category[]> {
+    return this.httpclient.get<Category[]>(this.REST_API_SERVER + '/api/categories');
   }
 }
